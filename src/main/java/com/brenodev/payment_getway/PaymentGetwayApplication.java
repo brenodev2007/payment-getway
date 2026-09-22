f531e440fcb9f3d65e8f51199ad68a24694466c3
Breno <breno.soriani@gmail.com>
2026-09-22 16:13:34 -0300

    first commit

---
+++ src/main/java/com/brenodev/payment_getway/PaymentGetwayApplication.java
@@ -0,0 +1,13 @@
+package com.brenodev.payment_getway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentGetwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentGetwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brenodev/payment_getway/PaymentGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brenodev.payment_getway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGetwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
